202a0305660314d737d4ff83dd402200246173e2
Atsedemariam <latseden@gmail.com>
2022-08-08 16:56:26 +0300

    first commit

---
+++ trial/initial.py
@@ -0,0 +1,12 @@
+#  function call
+
+def greet(name):
+    """
+    This function greets to
+    the person passed in as
+    a parameter.
+    """
+    print("Hello, " + name + ". Good morning!")
+
+# Error: name 'greet' is not defined
+greet('Atsedemariam')
